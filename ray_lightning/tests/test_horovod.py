fb227cd4d65f7da665463aa6e19b77a26ac7ca56
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-04-02 13:37:20 -0700

    update

--- ray_lightning/tests/test_horovod.py
+++ ray_lightning/tests/test_horovod.py
@@ -79,7 +79,7 @@ def test_predict(tmpdir, ray_start_2_cpus, seed, num_slots):
         data_dir=tmpdir, num_workers=1, batch_size=config["batch_size"])
     plugin = HorovodRayPlugin(num_slots=num_slots, use_gpu=False)
     trainer = get_trainer(
-        tmpdir, limit_train_batches=10, max_epochs=1, plugins=[plugin])
+        tmpdir, limit_train_batches=20, max_epochs=1, plugins=[plugin])
     predict_test(trainer, model, dm)
 
 
@@ -128,7 +128,7 @@ def test_predict_gpu(tmpdir, ray_start_2_gpus, seed, num_slots):
     plugin = HorovodRayPlugin(num_slots=num_slots, use_gpu=True)
     trainer = get_trainer(
         tmpdir,
-        limit_train_batches=10,
+        limit_train_batches=20,
         max_epochs=1,
         plugins=[plugin],
         use_gpu=True)
