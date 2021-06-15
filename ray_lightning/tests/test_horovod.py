66a9d16326059c6d7cbfebb36ef61e0a37e31a8a
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-06-15 09:03:05 -0700

    update

--- ray_lightning/tests/test_horovod.py
+++ ray_lightning/tests/test_horovod.py
@@ -3,6 +3,7 @@ import torch
 
 from pl_bolts.datamodules.mnist_datamodule import MNISTDataModule
 import pytorch_lightning as pl
+from ray.util.client.ray_client_helpers import ray_start_client_server
 
 try:
     import horovod  # noqa: F401
@@ -41,6 +42,12 @@ def ray_start_2_gpus():
     yield address_info
     ray.shutdown()
 
+@pytest.fixture
+def start_ray_client_server_2_cpus():
+    ray.init(num_cpus=2)
+    with ray_start_client_server() as client:
+        yield client
+
 
 @pytest.fixture
 def seed():
@@ -50,6 +57,15 @@ def seed():
 @pytest.mark.parametrize("num_slots", [1, 2])
 def test_train(tmpdir, ray_start_2_cpus, seed, num_slots):
     """Tests if training modifies model weights."""
+    model = BoringModel()
+    plugin = HorovodRayPlugin(num_slots=num_slots, use_gpu=False)
+    trainer = get_trainer(tmpdir, plugins=[plugin])
+    train_test(trainer, model)
+
+@pytest.mark.parametrize("num_slots", [1, 2])
+def test_train_client(tmpdir, start_ray_client_server_2_cpus, seed, num_slots):
+    """Tests if training modifies model weights."""
+    assert ray.util.client.ray.is_connected()
     model = BoringModel()
     plugin = HorovodRayPlugin(num_slots=num_slots, use_gpu=False)
     trainer = get_trainer(tmpdir, plugins=[plugin])
@@ -68,6 +84,23 @@ def test_load(tmpdir, ray_start_2_cpus, seed, num_slots):
 @pytest.mark.parametrize("num_slots", [1, 2])
 def test_predict(tmpdir, ray_start_2_cpus, seed, num_slots):
     """Tests if trained model has high accuracy on test set."""
+    config = {
+        "layer_1": 32,
+        "layer_2": 32,
+        "lr": 1e-2,
+        "batch_size": 32,
+    }
+    model = LightningMNISTClassifier(config, tmpdir)
+    dm = MNISTDataModule(
+        data_dir=tmpdir, num_workers=1, batch_size=config["batch_size"])
+    plugin = HorovodRayPlugin(num_slots=num_slots, use_gpu=False)
+    trainer = get_trainer(
+        tmpdir, limit_train_batches=20, max_epochs=1, plugins=[plugin])
+    predict_test(trainer, model, dm)
+
+@pytest.mark.parametrize("num_slots", [1, 2])
+def test_predict_client(tmpdir, start_ray_client_server_2_cpus, seed, num_slots):
+    assert ray.util.client.ray.is_connected()
     config = {
         "layer_1": 32,
         "layer_2": 32,
