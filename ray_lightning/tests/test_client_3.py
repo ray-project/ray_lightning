ef550d658aa76a2311c998fb752ae987909feb50
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-06-22 00:27:24 -0700

    split to separate files

--- ray_lightning/tests/test_client_3.py
+++ ray_lightning/tests/test_client_3.py
@@ -14,17 +14,28 @@ def start_ray_client_server_2_cpus():
         yield client
 
 
-def test_ddp_example(start_ray_client_server_2_cpus):
+def test_horovod_example(start_ray_client_server_2_cpus):
     assert ray.util.client.ray.is_connected()
-    from ray_lightning.examples.ray_ddp_example import train_mnist
+    from ray_lightning.examples.ray_horovod_example import train_mnist
     data_dir = os.path.join(tempfile.gettempdir(), "mnist_data_")
     config = {"layer_1": 32, "layer_2": 64, "lr": 1e-1, "batch_size": 32}
-    train_mnist(config, data_dir, num_epochs=1, num_workers=1, use_gpu=False)
+    train_mnist(
+        config,
+        data_dir,
+        num_epochs=1,
+        num_hosts=1,
+        num_slots=1,
+        use_gpu=False)
 
 
-def test_ddp_example_tune(start_ray_client_server_2_cpus):
+def test_horovod_example_tune(start_ray_client_server_2_cpus):
     assert ray.util.client.ray.is_connected()
-    from ray_lightning.examples.ray_ddp_example import tune_mnist
+    from ray_lightning.examples.ray_horovod_example import tune_mnist
     data_dir = os.path.join(tempfile.gettempdir(), "mnist_data_")
     tune_mnist(
-        data_dir, num_samples=1, num_epochs=1, num_workers=1, use_gpu=False)
+        data_dir,
+        num_samples=1,
+        num_epochs=1,
+        num_hosts=1,
+        num_slots=1,
+        use_gpu=False)
