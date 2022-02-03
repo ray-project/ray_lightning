3adb809aee8d1c6154e044902d359a456f1859ff
Amog Kamsetty <amogkam@users.noreply.github.com>
2022-02-02 16:22:31 -0800

    Bump version for development (#122)
    * add annotations
    
    * bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="ray_lightning",
     packages=find_packages(where=".", include="ray_lightning*"),
-    version="0.1.2",
+    version="0.3.0",
     author="Ray Team",
     description="Ray distributed plugins for Pytorch Lightning.",
     long_description="Custom Pytorch Lightning distributed plugins "
