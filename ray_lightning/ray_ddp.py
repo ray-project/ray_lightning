3df599a8bb1ac917bf6352b8baef63ad64f21595
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-06-21 23:13:56 +0000

    nit

--- ray_lightning/ray_ddp.py
+++ ray_lightning/ray_ddp.py
@@ -422,26 +422,7 @@ class RayPlugin(DDPSpawnStrategy):
         self._launcher = RayLauncher(self)
 
     def setup(self, trainer: "pl.Trainer") -> None:
-        super().setup(trainer)
-
-    def setup_environment(self) -> None:
-        # Swap out the accelerator if necessary.
-        # This is needed to support CPU head with GPU workers or Ray Client.
-        # current_accelerator = self.lightning_module.trainer.accelerator
-        # if self.use_gpu and isinstance(current_accelerator, CPUAccelerator):
-        #     from weakref import proxy
-        #     from ray_lightning.util import DelayedGPUAccelerator
-        #     precision_plugin = current_accelerator.precision_plugin
-        #     new_accelerator = DelayedGPUAccelerator(
-        #         precision_plugin=precision_plugin, training_type_plugin=self)
-        #     self.lightning_module.trainer._accelerator_connector \
-        #         ._training_type_plugin = \
-        #         proxy(new_accelerator.training_type_plugin)
-        #     self.lightning_module.trainer._accelerator_connector \
-        #         ._precision_plugin = proxy(new_accelerator.precision_plugin)
-        #     self.lightning_module.trainer._accelerator_connector.accelerator \
-        #         = new_accelerator
-                
+        super().setup(trainer)                
 
     def set_remote(self, remote: bool):
         self._is_remote = remote
