8e4bd9d5749207e5f762a35239c43e9040fde5a3
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-06-22 17:55:06 +0000

    adding the shard ddp

--- ray_lightning/ray_ddp_sharded.py
+++ ray_lightning/ray_ddp_sharded.py
@@ -1,6 +1,5 @@
-from pytorch_lightning.strategies import DDPSpawnShardedPlugin
-from pytorch_lightning.strategies.precision.sharded_native_amp import \
-    ShardedNativeMixedPrecisionPlugin
+from pytorch_lightning.strategies import DDPSpawnShardedStrategy
+
 from pytorch_lightning.utilities import _FAIRSCALE_AVAILABLE
 
 if _FAIRSCALE_AVAILABLE:
@@ -10,25 +9,21 @@ from ray.util import PublicAPI
 
 from ray_lightning import RayStrategy
 
+from pytorch_lightning.strategies.ddp_spawn import DDPSpawnStrategy
+from pytorch_lightning.utilities.imports import _FAIRSCALE_AVAILABLE
+
+if _FAIRSCALE_AVAILABLE:
+    from fairscale.nn.data_parallel.sharded_ddp import ShardedDataParallel
+    from fairscale.optim import OSS
+
+    from pytorch_lightning.overrides.fairscale import LightningShardedDataParallel, unwrap_lightning_module_sharded
+else:
+    OSS = ShardedDataParallel = object
+
+
 
 # C3 linearization of parent classes will do breadth first since both
-# RayStrategy and DDPSpawnShardedPlugin share a common parent of DDPSpawnPlugin
+# RayStrategy and DDPSpawnShardedStrategy share a common parent of DDPSpawnStrategy
 @PublicAPI(stability="beta")
-class RayShardedPlugin(RayStrategy, DDPSpawnShardedPlugin):
-    def execute_remote(self, model, global_rank, queue):
-        # Need to set self._model here otherwise self.lightning_module will
-        # return None.
-        self._model = model
-
-        # This is copied from `DDPSpawnShardedPlugin.new_process`.
-        # As of PTL 1.5, this is the only difference between
-        # `DDPSpawnShardedPlugin` and `DDPSpawnPlugin`.
-        precision_plugin = self.lightning_module.trainer.accelerator\
-            .precision_plugin
-        if isinstance(precision_plugin, ShardedNativeMixedPrecisionPlugin):
-            precision_plugin.scaler = ShardedGradScaler()
-
-        # After setting the grad scaler, we can now call the default
-        # `RayStrategy.execute_remote`.
-        return super().execute_remote(
-            model=self._model, global_rank=global_rank, queue=queue)
+class RayShardedStrategy(RayStrategy, DDPSpawnShardedStrategy):
+    strategy_name = "ddp_sharded_ray"
