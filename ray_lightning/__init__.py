cbded2053a4564d59f6d39f5967acf7baa0635de
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-06-22 16:52:37 +0000

    adding the ray launcher

--- ray_lightning/__init__.py
+++ ray_lightning/__init__.py
@@ -1,5 +1,6 @@
 from ray_lightning.ray_ddp import RayStrategy
 from ray_lightning.ray_horovod import HorovodRayPlugin
 from ray_lightning.ray_ddp_sharded import RayShardedPlugin
+from ray_lightning.ray_launcher import RayLauncher
 
-__all__ = ["RayStrategy", "HorovodRayPlugin", "RayShardedPlugin"]
+__all__ = ["RayStrategy", "HorovodRayPlugin", "RayShardedPlugin", "RayLauncher"]
