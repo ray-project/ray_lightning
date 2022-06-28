e95fe808c679e8f25d53c4857be6354b7ed5f69b
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-06-28 18:05:02 +0000

    nit

--- ray_lightning/__init__.py
+++ ray_lightning/__init__.py
@@ -1,8 +1,10 @@
 from ray_lightning.ray_ddp import RayStrategy
-from ray_lightning.ray_horovod import HorovodRayPlugin
-from ray_lightning.ray_ddp_sharded import RayShardedPlugin
+from ray_lightning.ray_horovod import HorovodRayStrategy
+from ray_lightning.ray_ddp_sharded import RayShardedStrategy
 from ray_lightning.ray_launcher import RayLauncher
+from ray_lightning.ray_horovod_launcher import RayHorovodLauncher
+
 
 __all__ = [
-    "RayStrategy", "HorovodRayPlugin", "RayShardedPlugin", "RayLauncher"
+    "RayStrategy", "HorovodRayStrategy", "RayShardedStrategy", "RayLauncher", "RayHorovodLauncher"
 ]
