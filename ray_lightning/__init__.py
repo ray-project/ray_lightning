31ddd137a278e92659463a27d2e65819a4397ae7
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-06-29 22:23:11 +0000

    nit

--- ray_lightning/__init__.py
+++ ray_lightning/__init__.py
@@ -1,8 +1,8 @@
+from ray_lightning.ray_launcher import RayLauncher
+from ray_lightning.ray_horovod_launcher import RayHorovodLauncher
 from ray_lightning.ray_ddp import RayStrategy
 from ray_lightning.ray_horovod import HorovodRayStrategy
 from ray_lightning.ray_ddp_sharded import RayShardedStrategy
-from ray_lightning.ray_launcher import RayLauncher
-from ray_lightning.ray_horovod_launcher import RayHorovodLauncher
 
 
 __all__ = [
