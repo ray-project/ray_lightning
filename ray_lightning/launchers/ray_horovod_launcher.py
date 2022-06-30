d6b19673e404680741fc62e787f8c36b217d5fe0
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-06-30 21:51:19 +0000

    debug

--- ray_lightning/launchers/ray_horovod_launcher.py
+++ ray_lightning/launchers/ray_horovod_launcher.py
@@ -119,6 +119,7 @@ class RayHorovodLauncher(_Launcher):
         self._executor = executor
         self._strategy.executor = executor
 
+        print(ray.get(self._futures))
 
         executor.shutdown()
         results = process_results(self._futures, self.tune_queue)
