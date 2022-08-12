868688df1ab1fb75803f034709a2ed3bb92d2e12
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-08-12 01:05:08 +0000

    fix ci

--- ray_lightning/examples/ray_ddp_sharded_example.py
+++ ray_lightning/examples/ray_ddp_sharded_example.py
@@ -59,7 +59,7 @@ def train(data_dir, num_workers, use_gpu, batch_size, embed_dim, max_epochs,
     dm = MNISTDataModule(data_dir, batch_size=batch_size)
     dm.train_transforms = None
     dm.val_transforms = None
-    
+
     model = ImageGPT(
         embed_dim=embed_dim, layers=16, heads=4, vocab_size=32, num_pixels=28)
 
